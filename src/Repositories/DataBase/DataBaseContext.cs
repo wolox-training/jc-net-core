d27af373afa54a46fff87cee0a8b42afa24cecf2
Casanova569 <jose.casanova@wolox.com.ar>
2019-05-20 13:24:04 -0300

    kick off

---
+++ src/Repositories/DataBase/DataBaseContext.cs
@@ -0,0 +1,21 @@
+// src/Repositories/Database/DataBaseContext.cs
+
+#region Using
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+#endregion
+
+namespace MvcMovie.Repositories.Database
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) {}
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
