7d299b50d188400359ec58d7164385d52193b09b
Marcos Trucco <truccomarcos@gmail.com>
2019-05-20 13:19:55 -0300

    Test endpoint - Added test endpoint on home controller

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace src.Controllers
             return View();
         }
 
+        public IActionResult TEST()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
