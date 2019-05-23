2acbaff2464b65ce13117d7a890c0453b711f6ec
Casanova569 <jose.casanova@wolox.com.ar>
2019-05-23 13:35:04 -0300

    mvc-controller fixed errors3

--- src/Controllers/HelloWorld.cs
+++ src/Controllers/HelloWorld.cs
@@ -5,15 +5,14 @@ namespace src.Controllers
 {
     public class HelloWorldController : Controller
     {
+        public string Index()
+        {
+            return "This is my default action...Hello World";
+        }
 
-    public string Index()
-    {
-        return "This is my default action...Hello World";
-    }
-
-    public string Welcome()
-    {
-        return "This is the Welcome action method...Hello World!!! (Welcome)";
-    }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...Hello World!!! (Welcome)";
+        }
     }
 }
