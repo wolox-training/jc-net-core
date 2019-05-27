6b05a9e873d784e7d87eebfb4ea6206ac3d56a61
Casanova569 <jose.casanova@wolox.com.ar>
2019-05-27 11:58:42 -0300

    authentication in progress2

--- src/Migrations/20190527145537_MvcMovie.Designer.cs
+++ src/Migrations/20190527145537_MvcMovie.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Models;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace src.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20190527145537_MvcMovie")]
+    partial class MvcMovie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Repositories/DataBase/MvcMovieContext.cs
+++ src/Repositories/DataBase/MvcMovieContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie.Models
 {
-    public class MvcMovieContext : DbContext
+    public class MvcMovieContext : IdentityDbContext<IdentityUser>
     {
         public MvcMovieContext (DbContextOptions<MvcMovieContext> options): base(options){}
 
