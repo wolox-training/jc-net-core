8cd1bbd37122bdd45d8fef4157c889f625aef4fb
Casanova569 <jose.casanova@wolox.com.ar>
2019-06-03 09:48:15 -0300

    rating fixed errors

--- src/Controllers/MovieApp.cs
+++ src/Controllers/MovieApp.cs
@@ -95,7 +95,7 @@ namespace MovieApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("ID,Title,ReleaseDate,Genre,Price, Rating")]Movie movie)
+        public IActionResult Create(Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -119,7 +119,7 @@ namespace MovieApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public IActionResult Edit(int id, Movie movie)
         {
             if (ModelState.IsValid)
             {
