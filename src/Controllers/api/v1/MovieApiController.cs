afc83e00eea72698c8ed15c68b4b52e49da06ad4
Casanova569 <jose.casanova@wolox.com.ar>
2019-06-12 13:31:49 -0300

    add mail details-movie

---
+++ src/Controllers/api/v1/MovieApiController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using Mailer;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using MvcMovie.Repositories.Interfaces;
+
+namespace MvcMovie.Controllers
+{
+	[Route("api/v1/MovieApiController")]
+    public class MovieApiController : MovieAppController
+    {
+
+		private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailService _mailer;
+        
+        public MovieApiController(IUnitOfWork unitOfWork, IEmailService mailer) : base(unitOfWork, mailer)
+        {
+        }
+
+        [HttpPost("Details")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(EmailViewModel email)
+        {
+            EmailAddress to = new EmailAddress(){
+                Name = email.Name,
+                Address = email.Adress
+            };
+            EmailMessage mailMessage= new EmailMessage(){
+                Subject = email.Subject,
+                Content = email.Body,
+            };
+            mailMessage.ToAddresses.Add(to);
+            Mailer.Send(mailMessage);
+            return Ok("Success");
+        }
+
+
+    }
+}
--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -35,4 +35,39 @@
 
     });
 
+});
+
+$(function(){
+    var urlDetails = '/api/v1/MovieApiController/Details';
+    $('#emailDetailsBtm').click(function() {
+        var mail = new Object();
+        mail.Name = $('#nameDetails').val();
+        mail.Adress = $('#adressDetails').val();
+        mail.Body = ('Id: ' + $('[name=detailsMovieId]').val() + ' Title: ' + $('[name=detailsMovieTitle]').val() + ' Release Date: ' + $('[name=detailsMovieReleaseDate]').val() + ' Genre: ' + $('[name=detailsMovieGenre]').val() + ' Price: ' + $('[name=detailsMoviePrice]').val() + ' Rating: ' + $('[name=detailsMovieRating]').val() );
+        mail.Subject = ('Information of the movie ' + $('[name=detailsMovieTitle]').val());
+        alert(mail.Body);
+        var tokenVal = $('[name=__RequestVerificationToken]').val();
+        $.ajax({  
+            type: "POST",  
+            url: urlDetails,  
+            data: {
+                __RequestVerificationToken: tokenVal,
+                email : mail
+            },
+            success: function(response) {  
+                if (response != null) {  
+                    alert("The mail was send succesfully");
+                } else {  
+                    alert("Something went wrong");  
+                }  
+            },  
+            failure: function(response) {  
+                alert('fail');  
+            },  
+            error: function(response,status,error) {  
+                alert('Status: ' + status + ' Error: ' + error);   
+            }  
+        }); 
+    });
+
 }); 
